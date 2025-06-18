b6b0885c4f51191eff6889a5fd2c6c47e6c3e8d2
sy11219 <syr_1925@outlook.com>
2025-06-18 03:14:56 -0500

    datosentrega y metodopago (aún no lo agrega a la bd pero ya casi)

--- JMQPresentacion/Pedidos/DatosEntrega.aspx.cs
+++ JMQPresentacion/Pedidos/DatosEntrega.aspx.cs
@@ -10,13 +10,14 @@ namespace JMQPresentacion.Pedidos
 {
     public partial class DatosEntrega : System.Web.UI.Page
     {
+        private EntregaWSClient entregaService;
         protected void Page_Init(object sender, EventArgs e)
         {
             if (Session["usuario"] == null)
             {
                 Response.Redirect("~/Login.aspx");
             }
-
+            entregaService = new JMQWS.EntregaWSClient();
         }
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -30,7 +31,7 @@ namespace JMQPresentacion.Pedidos
         }
 
         private void CargarResumen()
-    {
+        {
             List<detalle> detalles = (List<detalle>)Session["Cart"];
             lblTotal.Text = "S/ " + detalles.Sum(item => item.cantidad * item.precio_unitario).ToString("F2");
             lblTotal2.Text = lblTotal.Text;
@@ -39,27 +40,51 @@ namespace JMQPresentacion.Pedidos
         protected void btnPagar_Click(object sender, EventArgs e)
         {
 
-            if (Session["Usuario"] == null)
+            if (Session["usuario"] == null)
             {
                 Response.Redirect("~/Login/Login.aspx");
             }
             else
             {
-
-                //entrega entrega1 = new entrega
-                //{
-                //    orden = (ordenVenta)Session["OrdenVenta"],
-                //    fechaEntrega = DateTime.Now, //?
-                //    tipoEntrega = pnlDespacho.Visible ? tipoEntrega.DELIVERY : tipoEntrega.RECOJO
-                //};
-                //if (pnlDespacho.Visible)
-                //    entrega1.direccion = $"{txtDireccion.Text} {txtNumero.Text} {txtPisoDpto.Text} {txtReferencia.Text}";
-                //else
-                //    entrega1.dniRecibo = txtDni.Text;
-                // insertar Entrega a la BD
-                // insertar(entrega);
+                divError.Style["display"] = "none";
+                lblError.Text = "";
+                //comprobaciones
+                if ((pnlDespacho.Visible && (string.IsNullOrWhiteSpace(txtDireccion.Text) || string.IsNullOrWhiteSpace(txtNumero.Text))) ||
+                    (pnlRetiro.Visible && string.IsNullOrWhiteSpace(txtDni.Text)))
+                {
+                    lblError.Text = "Complete todos los campos obligatorios.";
+                    divError.Style["display"] = "block";
+                    return;
+                }
+                if (pnlRetiro.Visible && txtDni.Text.Length != 8)
+                {
+                    lblError.Text = "DNI inválido.";
+                    divError.Style["display"] = "block";
+                    return;
+                }
+                if (pnlDespacho.Visible && (!txtNumero.Text.All(char.IsDigit) || int.Parse(txtNumero.Text) < 0))
+                {
+                    lblError.Text = "Ingrese un número de dirección válido.";
+                    divError.Style["display"] = "block";
+                    return;
+                }
+                entrega entrega1 = new entrega
+                {
+                    orden = (ordenVenta)Session["Orden"],
+                    fecha_entrega = DateTime.Now.AddDays(7),
+                    tipoEntrega = pnlDespacho.Visible ? tipoEntrega.DELIVERY : tipoEntrega.RECOJO
+                };
+                if (pnlDespacho.Visible)
+                {
+                    string direccion = string.Join(" ", new[] { txtDireccion.Text, txtNumero.Text, txtPisoDpto.Text, txtReferencia.Text }
+                        .Where(s => !string.IsNullOrWhiteSpace(s)));
+                    entrega1.direccion = direccion;
+                }
+                else
+                    entrega1.dniRecibo = txtDni.Text;
+                //insertar Entrega a la BD
+                //entregaService.RegistrarEntrega(entrega1);
                 Response.Redirect("~/Pedidos/MetodoPago.aspx");
-                //}
             }
         }
 
--- JMQPresentacion/Pedidos/MetodoPago.aspx.cs
+++ JMQPresentacion/Pedidos/MetodoPago.aspx.cs
@@ -10,15 +10,11 @@ namespace JMQPresentacion.Pedidos
 {
     public partial class MetodoPago : System.Web.UI.Page
     {
+        private ComprobantePagoWSClient comprobanteService;
         protected void Page_Init(object sender, EventArgs e)
         {
-            if (Session["usuario"] == null)
-            {
-                Response.Redirect("~/Login.aspx");
-            }
-
+            comprobanteService = new ComprobantePagoWSClient();
         }
-
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -32,9 +28,9 @@ namespace JMQPresentacion.Pedidos
 
         private void CargarResumen()
         {
-            //List<Detalle> detalles = (List<Detalle>)Session["Cart"];
-            //lblTotal.Text = "S/ " + detalles.Sum(item => item.cantidad * item.precio_unitario).ToString("F2");
-            //lblTotal2.Text = lblTotal.Text;
+            List<detalle> detalles = (List<detalle>)Session["Cart"];
+            lblTotal.Text = "S/ " + detalles.Sum(item => item.cantidad * item.precio_unitario).ToString("F2");
+            lblTotal2.Text = lblTotal.Text;
         }
         protected void MetodoPago_Changed(object sender, EventArgs e)
         {
@@ -44,19 +40,70 @@ namespace JMQPresentacion.Pedidos
 
         protected void btnPagar_Click(object sender, EventArgs e)
         {
-            /*
+
             if (Session["Usuario"] == null)
             {
                 Response.Redirect("~/Login/Login.aspx");
             }
             else
             {
-            */
-                //MetodoPago metodo;
-                // insertar Entrega a la BD
-                // insertar(metodo);
-                Response.Redirect("~/Pedidos/MetodoPago.aspx");
-            //}
+                string textoSeleccionado = rblComprobante.SelectedItem.Text;
+                if (textoSeleccionado == "Factura")
+                {
+                    try
+                    {
+                        factura comprobante = new factura
+                        {
+                            RUC = ((usuario)Session["Usuario"]).RUC,
+                            razon_social = ((usuario)Session["Usuario"]).razonsocial,
+                            direccion = ((usuario)Session["Usuario"]).direccion,
+                            fecha_emision = DateTime.Now,
+                            orden = (ordenVenta)Session["Orden"],
+                            metodoPago = metodoPago.tarjeta, //rbVisa.Checked ? metodoPago.tarjeta : metodoPago.efectivo,
+                            fecha_pago = DateTime.Now,
+                            monto_total = ((List<detalle>)Session["Cart"]).Sum(item => item.cantidad * item.precio_unitario),
+                        };
+                        comprobanteService.registrarComprobante(comprobante);
+                    }
+                    catch
+                    {
+                        divError.Style["display"] = "block";
+                        lblError.Text = "Error al realizar el pago.";
+                        return;
+                    }
+
+                }
+                else if (textoSeleccionado == "Boleta")
+                {
+                    try
+                    {
+                        boleta comprobante = new boleta
+                        {
+                            dni = ((usuario)Session["Usuario"]).dni,
+                            nombre = ((usuario)Session["Usuario"]).nombreUsuario,
+                            fecha_emision = DateTime.Now,
+                            orden = (ordenVenta)Session["Orden"],
+                            metodoPago = metodoPago.tarjeta, //rbVisa.Checked ? metodoPago.tarjeta : metodoPago.efectivo,
+                            fecha_pago = DateTime.Now,
+                            monto_total = ((List<detalle>)Session["Cart"]).Sum(item => item.cantidad * item.precio_unitario),
+                        };
+                        //comprobanteService.registrarComprobante(comprobante);
+                    }
+                    catch
+                    {
+                        divError.Style["display"] = "block";
+                        lblError.Text = "Error al realizar el pago.";
+                        return;
+                    }
+                }
+                else
+                {
+                    divError.Style["display"] = "block";
+                    lblError.Text = "Seleccione un tipo de comprobante válido.";
+                    return;
+                }
+                Response.Redirect("/Principal/Principal.aspx");
+            }
         }
     }
 }
